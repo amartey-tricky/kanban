66846243716c9487ff3da67e0003449053e23c6f
Tricky <trickya23@outlook.com>
2025-06-19 23:04:53 +0000

    tired

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import KanbanProjectApp from "@/components/home"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <KanbanProjectApp />
+      <div className="flex items-center justify-between w-full max-w-5xl mb-8">
+        <KanbanProjectApp />
+      </div>
     </main>
   )
 }
