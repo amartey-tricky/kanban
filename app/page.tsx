6352988a38d5b0a6077d3916d9dd727b81568f0a
Tricky <trickya23@outlook.com>
2025-06-19 23:20:33 +0000

    fix home page 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import KanbanProjectApp from "@/components/home"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen items-center justify-between">
       <KanbanProjectApp />
     </main>
   )
